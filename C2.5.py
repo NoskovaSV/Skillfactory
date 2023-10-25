751e40db3c5dcca9863edf771375eb10efaa145a
Светлана Носкова <142382581+NoskovaSV@users.noreply.github.com>
2023-10-25 12:34:51 +0300

    New update C2.5.py

--- C2.5.py
+++ C2.5.py
@@ -10,7 +10,6 @@ class Dot:
             return self.x == other.x and self.y == other.y
         return False
 
-
 class Ship:
     def __init__(self, length, bow_dot, ship_direction, life_quantity):
         self.length = length
@@ -27,24 +26,28 @@ class Ship:
             else:
                 dot = Dot(self.bow_dot.x, self.bow_dot.y + i)
             dot_list.append(dot)
-            return dot_list
+        return dot_list
 
 
 class Board:
-    def __init__(self, field, ship_lst, hid, live_ships):
+    def __init__(self, hid, live_ships):
         self.field = [['0'] * 6 for _ in range(6)]
         self.ship_lst = []
         self.hid = hid
         self.live_ships = live_ships
 
     def add_ship(self, ship):
+        ship.dots()
         for dot in ship.dots:
             if self.out(dot):
                 raise Exception
-            else:
-                for dot in ship.dots:
-                    self.field[dot.x][dot.y] = "■"
-            self.ship_lst.append(ship)
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if (dot.x + i, dot.y + j):
+                    raise Exception
+            if dot.x and dot.y in self.ship_lst:
+                self.ship_lst.append(ship)
+                print("■")
 
     def out(self, dot):
         if dot.x < 0 or dot.y < 0 or dot.x >= 6 or dot.y >= 6:
@@ -57,7 +60,7 @@ class Board:
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     new_dot = Dot(dot.x + i, dot.y + j)
-                    if self.is_out(new_dot) or new_dot == dot:
+                    if self.out(new_dot) or new_dot == dot:
                         continue
                     contour_dots.append(new_dot)
         return contour_dots
@@ -67,29 +70,16 @@ class Board:
         for i, row in enumerate(self.field, start=1):
             print(i, *row)
 
-    def make_shot(self, dot, shot):
-        try:
-            if dot <= 0 or dot >= 6:
-                except Exception as e:
-            print(e)
-        try:
-            elif dot == "X":
-            except ShotError as e:
-        print("Сюда нельзя произвести выстрел")
-        elif x or y== 0 and shot == "T":
-        print("Промах")
-        return False
-
-        elif dot == "X":
-        for ship in ship_lst:
-        x += 1
-        y += 1
-        elif ship_health == 0:
-        print("Корабль потоплен")
-        return True
+    def make_shot(self, shot):
+        if self.out:
+            raise Exception
+        elif self.field[dot.x][dot.y] == "T":
+            raise Exception
+        elif self.field[dot.x][dot.y] == '■':
+            self.field[dot.x][dot.y] = "X"
+            self.live_ships -= 1
         else:
-        print("Корабль ранен")
-        return True
+            self.field[dot.x][dot.y] = "T"
 
 
 class Player:
@@ -100,28 +90,26 @@ class Player:
     def ask(self):
         pass
 
-    def board_shot(self, shot_point):
-        try:
-
-        except Exception as e:
-            print(e)
-
     def move(self):
         self.ask()
 
+    def board_shot(self, shot_point):
+        if shot_point == "T":
+            raise Exception
+        elif shot_point == "X":
+            for dot in ship.dots():
+                print("Корабль подбит. Сделайте повторный выстрел!")
+            return True
 
-class AI:
+
+class AI(Player):
     def ask(self):
-        print("В какую клетку сделать выстрел?")
+        print("В какую клетку ИИ сделает выстрел?")
 
 
-class User:
+class User(Player):
     def ask(self):
-        print("В какую клетку сделать выстрел?")
-
-
-human = Ship()
-computer = Ship()
+        print("В какую клетку игрок сделает выстрел?")
 
 
 class Game:
@@ -132,17 +120,38 @@ class Game:
         ai = AI()
 
     def random_board(self):
-        pass
+        random.randint(1, 36)
+        while dot in self.field == "X":
+            dot = dot + 1
+        else:
+            print("На эту доску невозможно поставить корабль")
 
-    def greet(self):
-        print("Добро пожаловать в игру Морской бой!")
 
-    def loop(self):
-        self.mode
+def greet(self):
+    print("Добро пожаловать в игру Морской бой!")
 
-    def start(self):
-        self.greet("")
-        self.loop("")
 
-    Game = Game()
-    Game.start
+def loop(self):
+    self.mode()
+
+    class Ai(Player):
+        def verify(self):
+            super().verify()
+            if self.live_ships == 0:
+                print("ИИ выиграл!")
+
+    class User(Player):
+        def verify(self):
+            super().verify()
+
+        if self.live_ships == 0:
+            print("Игрок выиграл!")
+
+
+def start(self):
+    self.greet()
+    self.loop()
+
+
+new_game = Game()
+Game.start()
