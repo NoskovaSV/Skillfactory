add2c8bc7de23d663569c02273f6e980b9abac78
Светлана Носкова <142382581+NoskovaSV@users.noreply.github.com>
2023-11-28 15:23:17 +0300

    Second commit

---
+++ NewsPortal/NewsPortal/news/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostList(ListView):
+    model=Post
+    ordering='-creation_date'
+    template_name='posts.html'
+    context_object_name ='posts'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'specific_post.html'
+    context_object_name = 'post'
+
+
+
