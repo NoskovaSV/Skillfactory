de534b1c2111774eb3a79ddcefccb3870cacdce1
Светлана Носкова <142382581+NoskovaSV@users.noreply.github.com>
2023-12-04 17:14:09 +0300

    Third commit

--- NewsPortal/NewsPortal/news/models.py
+++ NewsPortal/NewsPortal/news/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
 from django.db.models.functions import Coalesce
+from django.core.validators import MinValueValidator
+from django.urls import reverse
 
 class Author(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -16,19 +18,22 @@ class Author(models.Model):
         self.rating=post_rating*3+comment_rating+post_comment_rating
         self.save()
 
+    def __str__(self):
+        return self.user.username
+
 class Category(models.Model):
     category_name = models.CharField(max_length=255, default="Default value", unique=True)
 
     def __str__(self):
-        return self.caregory_name.title()
+        return self.category_name.title()
 
 class Post(models.Model):
     user = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="post")
     A = "article"
     N = "news"
-    choices = ((A, "Статья")), (N, "Новость")
+    choices = ((A, "Статья"), (N, "Новость"))
     choice_field = models.CharField(max_length=7, choices=choices, default="news")
-    creation_date = models.DateField(auto_now_add=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
     categories = models.ManyToManyField(Category, through="PostCategory")
     header = models.CharField(max_length=255, default="Default value")
     text = models.TextField()
@@ -45,6 +50,9 @@ class Post(models.Model):
     def preview(self):
         preview = self.text[124:]
         return preview
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- NewsPortal/NewsPortal/news/urls.py
+++ NewsPortal/NewsPortal/news/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
     path('', PostList.as_view()),
-    path('<int:pk>', PostDetail.as_view()),
+    path('search/', PostList.as_view()),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('create/', PostCreate.as_view(), name='news_create'),
+    path('<int:pk>/edit/', PostUpdate.as_view(), name='news_edit'),
+    path('<int:pk>/delete/', PostDelete.as_view(), name='news_delete'),
+    path('articles/create/', PostCreate.as_view(), name='articles_create'),
+    path('articles/<int:pk>/edit/', PostUpdate.as_view(), name='articles_edit'),
+    path('articles/<int:pk>/delete/', PostDelete.as_view(), name='articles_delete'),
+
 ]
+
 
